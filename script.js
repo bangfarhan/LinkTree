aad16a7bf2dfcd95dc71fe52b8925cf8c6d3458b
Farhan Ma'arif Lubis <59276977+bangfarhan@users.noreply.github.com>
2020-11-26 07:37:14 +0500

    Link Tree
    Project 01 Mandiri

---
+++ script.js
@@ -0,0 +1,18 @@
+function fb() {
+    document.location   = 'https://www.facebook.com/lubishans';
+}
+function ig() {
+    document.location   = 'https://www.instagram.com/farrhanlubis?r=nametag';
+}
+function wa() {
+    document.location   = 'https://api.whatsapp.com/send?phone=6282119011484';
+}
+function twit() {
+    document.location   = 'https://www.twitter.com/FarhanMaarifLu4?s=09';
+}
+function yt() {
+    document.location   = 'https://www.youtube.com/channel/UCEghTJbyrRZxqF6XUIFLug';
+}
+function github() {
+    document.location   = 'https://www.github.com/bangfarhan';
+}
